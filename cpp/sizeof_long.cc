0d44cb88273f139103954c4bb781ea6fae52e698
shaojwa <shaojwa@gmail.com>
2021-07-17 05:51:41 +0000

    add sourcs to lang

---
+++ cpp/sizeof_long.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "sizeof(long) " << sizeof(long) << endl;
+	cout << "sizeof(long long) " << sizeof(long long) << endl;
+	return 0;
+}
